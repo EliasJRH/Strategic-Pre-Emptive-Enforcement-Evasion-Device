2d979c051c649df99d9a4516d178aa60cbe5af3b
Elias Hawa <elias.hawa2468@gmail.com>
2025-04-23 11:14:00 -0400

    Initial commit

---
+++ Strategic-Pre-Emptive-Enforcement-Evasion-Device.ino
@@ -0,0 +1,113 @@
+#include <TinyGPSPlus.h>
+#include <SoftwareSerial.h>
+/*
+   This sample sketch demonstrates the normal use of a TinyGPSPlus (TinyGPSPlus) object.
+   It requires the use of SoftwareSerial, and assumes that you have a
+   4800-baud serial GPS device hooked up on pins 4(rx) and 3(tx).
+*/
+static const int RXPin = 16, TXPin = 14;
+static const uint32_t GPSBaud = 9600;
+const int PiezoPin = 8;
+
+// The TinyGPSPlus object
+TinyGPSPlus gps;
+
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
+
+void setup()
+{
+  Serial.begin(115200);
+  ss.begin(GPSBaud);
+
+  Serial.println(F("DeviceExample.ino"));
+  Serial.println(F("A simple demonstration of TinyGPSPlus with an attached GPS module"));
+  Serial.print(F("Testing TinyGPSPlus library v. ")); Serial.println(TinyGPSPlus::libraryVersion());
+  Serial.println(F("by Mikal Hart"));
+  Serial.println();
+}
+
+void loop()
+{
+  // This sketch displays information every time a new sentence is correctly encoded.
+  while (ss.available() > 0)
+    if (gps.encode(ss.read()))
+      displayInfo();
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    Serial.println(F("No GPS detected: check wiring."));
+    while(true);
+  }
+}
+
+void displayInfo()
+{
+  Serial.print(F("Location: ")); 
+  if (gps.location.isValid())
+  {
+    Serial.print(gps.location.lat(), 10);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 10);
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F("  Date/Time: "));
+  if (gps.date.isValid())
+  {
+    Serial.print(gps.date.month());
+    Serial.print(F("/"));
+    Serial.print(gps.date.day());
+    Serial.print(F("/"));
+    Serial.print(gps.date.year());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F(" "));
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.hour());
+    Serial.print(F(":"));
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.minute());
+    Serial.print(F(":"));
+    if (gps.time.second() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.second());
+    Serial.print(F("."));
+    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.centisecond());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+  Serial.print(" Speed: ");
+  if (gps.speed.isValid())
+  {
+    Serial.print(gps.speed.kmph());
+    Serial.print(" km/h");
+  }
+  else
+  {
+    Serial.print("INVALID");
+  }
+  float bronson_lat = 45.3174384885;
+  float bronson_lng = -75.6020732936;
+  float dist_to_bronson = gps.distanceBetween(gps.location.lat(), gps.location.lng(), bronson_lat, bronson_lng);
+  Serial.print(" Distance from Bronson speed cam: ");
+  Serial.print(dist_to_bronson);
+  Serial.print(" m ");
+  // tone(PiezoPin, 2000, 500);
+  //tone(PiezoPin, 2000, 1000);
+  ///delay(1000);
+  //noTone(PiezoPin);
+
+  Serial.println();
+  delay(400);
+}
