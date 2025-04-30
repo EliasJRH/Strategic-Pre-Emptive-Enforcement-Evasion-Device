2cc0eaf52154c490f64d17ab2199ae7150a6bf75
Elias Hawa <elias.hawa2468@gmail.com>
2025-04-30 18:06:31 -0400

    Add proximity detection

--- Strategic-Pre-Emptive-Enforcement-Evasion-Device.ino
+++ Strategic-Pre-Emptive-Enforcement-Evasion-Device.ino
@@ -1,5 +1,6 @@
 #include <TinyGPSPlus.h>
 #include <SoftwareSerial.h>
+#include <binary.h>
 /*
    This sample sketch demonstrates the normal use of a TinyGPSPlus (TinyGPSPlus) object.
    It requires the use of SoftwareSerial, and assumes that you have a
@@ -8,6 +9,7 @@
 static const int RXPin = 16, TXPin = 14;
 static const uint32_t GPSBaud = 9600;
 const int PiezoPin = 8;
+const int LEDPin = 6;
 
 // The TinyGPSPlus object
 TinyGPSPlus gps;
@@ -15,10 +17,80 @@ TinyGPSPlus gps;
 // The serial connection to the GPS device
 SoftwareSerial ss(RXPin, TXPin);
 
+typedef struct {
+  double latitude;
+  double longitude;
+} coordPair;
+
+coordPair coords[] = {
+  {45.28146886, -75.74392084},
+  {45.46935075, -75.45873444},
+  {45.40021726, -75.63620026},
+  {45.43924462, -75.59942769},
+  {45.34462751, -75.74999215},
+  {45.47888292, -75.47825523},
+  {45.35109943, -75.80631488},
+  {45.30821623, -75.89970065},
+  {45.43322705, -75.5654689554},
+  {45.3762722479, -75.660905566},
+  {45.3340869419, -75.7838346302},
+  {45.2652023749, -75.9099980496},
+  {45.4384310938, -75.6453343887},
+  {45.4717851882, -75.4944270367},
+  {45.364651381, -75.7124151448},
+  {45.3230004136, -75.924709162},
+  {45.2949096036, -75.8813335527},
+  {45.2785866571, -75.8569792798},
+  {45.2777213039, -75.7793625171},
+  {45.272065083, -75.724560914},
+  {45.2852002063, -75.7095993577},
+  {45.2624552977, -75.7416666407},
+  {45.273377392, -75.9307772822},
+  {45.3645359225, -75.7701692356},
+  {45.3742208049, -75.773043905},
+  {45.254533189, -75.7326814156},
+  {45.4015010387, -75.6962570401},
+  {45.4576945841, -75.4659423485},
+  {45.3659607245, -75.6890686036},
+  {45.279552655, -75.738320142},
+  {45.3174384885, -75.6020732936},
+  {45.2816928193, -75.6864424388},
+  {45.2840564801, -75.8458194355},
+  {45.3611477547, -75.6285653299},
+  {45.3864254402, -75.6237983},
+  {45.3883969422, -75.6926336623},
+  {45.446483, -75.596159},
+  {45.3794690898, -75.654934766},
+  {45.4782729275, -75.540446514},
+  {45.4338303582, -75.6900702041},
+  {45.3265193065, -75.8079304761},
+  {45.3737706134, -75.6550544},
+  {45.3599059802, -75.6999508132},
+  {45.3042480616, -75.8957381499},
+  {45.4107477572, -75.6791235456},
+  {45.3278664331, -75.7676975637},
+  {45.1855972222, -75.8264130936},
+  {45.2888548066, -75.7176619303},
+  {45.4387795704, -75.6786160171},
+  {45.4688572975, -75.4737434457},
+  {45.2727214523, -75.7375774126},
+  {45.3683972856, -75.6556107143},
+  {45.3227529491, -75.7174008334},
+  {45.4250254351, -75.6555102065},
+  {45.454336379, -75.5980269984},
+  {45.2641669344, -75.9276239242},
+  {45.320561839, -75.8264788793},
+  {45.4221778042, -75.4170741213},
+  {45.2294525958, -75.6823655337},
+  {45.3396748612, -75.9398458559}
+};
+
 void setup()
 {
   Serial.begin(115200);
   ss.begin(GPSBaud);
+  pinMode(PiezoPin, OUTPUT);
+  pinMode(LEDPin, OUTPUT);
 
   Serial.println(F("DeviceExample.ino"));
   Serial.println(F("A simple demonstration of TinyGPSPlus with an attached GPS module"));
@@ -31,8 +103,37 @@ void loop()
 {
   // This sketch displays information every time a new sentence is correctly encoded.
   while (ss.available() > 0)
-    if (gps.encode(ss.read()))
-      displayInfo();
+    if (gps.encode(ss.read())){
+      // displayInfo();
+      float closestDist = -1;
+      coordPair closestPair;
+      for (int x = 0; x < 59; ++x){
+        Serial.print("Distance to ");
+        Serial.print(coords[x].latitude, 10);
+        Serial.print(", ");
+        Serial.print(coords[x].longitude, 10);
+        Serial.print(": ");
+        float dist = gps.distanceBetween(gps.location.lat(), gps.location.lng(), coords[x].latitude, coords[x].longitude);
+        Serial.println(dist);
+        if (closestDist == -1 || dist < closestDist){
+          closestDist = dist;
+          closestPair = coords[x];
+        } 
+      }
+      Serial.print("Closest camera: ");
+      Serial.print(closestPair.latitude, 10);
+      Serial.print(", ");
+      Serial.print(closestPair.longitude, 10);
+      Serial.print(": ");
+      Serial.println(closestDist, 10);
+      Serial.println("//////////");
+      if (closestDist < 400){
+        digitalWrite(LEDPin, HIGH);
+      } else{
+        digitalWrite(LEDPin, LOW);
+      }
+      delay(1000);
+    }
   if (millis() > 5000 && gps.charsProcessed() < 10)
   {
     Serial.println(F("No GPS detected: check wiring."));
@@ -100,14 +201,15 @@ void displayInfo()
   float bronson_lat = 45.3174384885;
   float bronson_lng = -75.6020732936;
   float dist_to_bronson = gps.distanceBetween(gps.location.lat(), gps.location.lng(), bronson_lat, bronson_lng);
-  Serial.print(" Distance from Bronson speed cam: ");
+  Serial.print(" Distance from Kelly Farm speed cam: ");
   Serial.print(dist_to_bronson);
   Serial.print(" m ");
-  // tone(PiezoPin, 2000, 500);
-  //tone(PiezoPin, 2000, 1000);
-  ///delay(1000);
-  //noTone(PiezoPin);
-
+  double course = gps.courseTo(gps.location.lat(), gps.location.lng(), bronson_lat, bronson_lng);
+  char* dir = gps.cardinal(course);
+  Serial.print(" Heading to Kelly Farm speed cam: ");
+  Serial.print(course);
+  Serial.print(" (");
+  Serial.print(dir);
+  Serial.print(") ");
   Serial.println();
-  delay(400);
 }
